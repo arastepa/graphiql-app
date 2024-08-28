fc3f4ba48183d2181396137ae186d72aa9349bb4
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-08-29 02:11:43 +0400

    fix: test error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import WelcomeSection from '../components/WelcomeSection';
+"use client";
+
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import WelcomeSection from "../components/WelcomeSection";
 // import UserSection from '../components/UserSection';
-import styles from './page.module.css';
+import styles from "./page.module.css";
+import { LanguageProvider } from "../context/LanguageContext";
 
 const HomePage: React.FC = () => {
-
-    return (
-        <div className={styles.page}>
-            <Header isAuthenticated={false} onSignOut={() => {}} />
-                <main className={styles.main}><WelcomeSection /></main>
-            <Footer />
-        </div>
-    );
+  return (
+    <LanguageProvider>
+      <div className={styles.page}>
+        <Header isAuthenticated={false} onSignOut={() => {}} />
+        <main className={styles.main}>
+          <WelcomeSection />
+        </main>
+        <Footer />
+      </div>
+    </LanguageProvider>
+  );
 };
 
 export default HomePage;
--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -1,27 +1,40 @@
 "use client";
-import React from 'react';
-import Image from 'next/image';
-import mainImage from '../../public/img/homeImg.png';
-import styles from '../styles/Welcome.module.css';
+import React from "react";
+import Image from "next/image";
+import mainImage from "../../public/img/homeImg.png";
+import styles from "../styles/Welcome.module.css";
+import { useTranslation } from "react-i18next";
 
 const WelcomeSection: React.FC = () => {
-    return (
-        <div className={styles.welcome}>
-            <h1 className={styles.welcomePart}>Welcome to Rest/Graphiql Client</h1>
-            <h4 className={styles.welcomeText}>Our platform supports various endpoints and allows you to input queries, variables, and headers.</h4>
-            <Image 
-                src={mainImage} 
-                alt="home img" 
-                width={600} 
-                height={337} 
-                className={styles.welcomeImg}
-            />
-            <div className={styles.welcomeButtons}>
-                <button className={styles.welcomeButton} onClick={() => console.log('Redirect to Sign In')}>Sign In</button>
-                <button className={styles.welcomeButton} onClick={() => console.log('Redirect to Sign Up')}>Sign Up</button>
-            </div>
-        </div>
-    );
+  const { t } = useTranslation();
+  return (
+    <div className={styles.welcome}>
+      <h1 className={styles.welcomePart}>{t(`Header.Title`)}</h1>
+      <h4 className={styles.welcomeText}>{t(`Header.Paragraph`)}</h4>
+      <Image
+        src={mainImage}
+        alt="home img"
+        priority
+        width={600}
+        height={337}
+        className={styles.welcomeImg}
+      />
+      <div className={styles.welcomeButtons}>
+        <button
+          className={styles.welcomeButton}
+          onClick={() => console.log("Redirect to Sign In")}
+        >
+          {t(`SignIn`)}
+        </button>
+        <button
+          className={styles.welcomeButton}
+          onClick={() => console.log("Redirect to Sign Up")}
+        >
+          {t(`SignUp`)}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default WelcomeSection;
