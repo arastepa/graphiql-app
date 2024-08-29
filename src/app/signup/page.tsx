f85dd77f9590b85a8bae3618104bc46ab811cd10
arastepa <stepanyan2298@gmail.com>
2024-08-29 23:26:48 +0400

    fix: finish signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,13 @@
-"use server";
+'use server';
 
-import SignUpForm from "@/components/SignUpForm";
-import styles from "@/styles/SignUp.module.css";
-import { validationSchemaSignUp } from "@/utils/validate";
-import * as Yup from "yup";
+import SignUpForm from '@/components/SignUpForm';
+import styles from '@/styles/SignUp.module.css';
+import { validationSchemaSignUp } from '@/utils/validate';
+import * as Yup from 'yup';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '@/firebase';
+import { redirect } from 'next/navigation';
+import { isRedirectError } from 'next/dist/client/components/redirect';
 
 export const handleSignup = async (prevState: unknown, data: FormData) => {
   try {
@@ -14,11 +18,18 @@ export const handleSignup = async (prevState: unknown, data: FormData) => {
     await validationSchemaSignUp.validate(formDataObject, {
       abortEarly: false,
     });
-    return { message: "aa" };
+    await createUserWithEmailAndPassword(
+      auth,
+      formDataObject.email as string,
+      formDataObject.password as string,
+    );
+    redirect('/');
   } catch (err) {
+    if (isRedirectError(err)) redirect('/');
     if (err instanceof Yup.ValidationError) {
       return { message: err.errors };
     }
+    console.log(err);
   }
 };
 const SignUp = () => {
