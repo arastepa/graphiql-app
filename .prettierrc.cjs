4aa42cec3228afcd46bc018359345f379901d1ab
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-08-30 19:59:18 +0400

    refactor: update page.test.tsx

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  singleQuote: true,
   semi: true,
   useTabs: false,
   tabWidth: 2,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Header from '@/components/Header';
-import { LanguageProvider } from '@/context/LanguageContext';
-import Footer from '@/components/Footer';
+import Header from '../components/Header';
+import { LanguageProvider } from '../context/LanguageContext';
+import Footer from '../components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
