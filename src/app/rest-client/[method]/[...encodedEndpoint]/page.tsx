391a1ca0931763e49676e0478aaf425feeb9d1ba
arastepa <stepanyan2298@gmail.com>
2024-09-05 18:30:19 +0400

    fix: working post request

---
+++ src/app/rest-client/[method]/[...encodedEndpoint]/page.tsx
@@ -0,0 +1,74 @@
+import { decode } from 'base64-url';
+import RestClient from '../../page';
+import styles from '@/styles/Resp.module.css';
+import ResponseSection from '@/components/ResponseSection';
+
+export default async function ResponsePage({
+  params,
+  searchParams,
+}: {
+  params: { method: string; encodedEndpoint: string[] }; // Add encodedBody here
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const method = params.method;
+  const endpoint = decode(params.encodedEndpoint[0]);
+  const encodedURL = params.encodedEndpoint;
+  const encodedBody = encodedURL[1];
+
+  const body =
+    method !== 'GET' && encodedBody
+      ? JSON.parse(decode(encodedBody))
+      : undefined;
+  const headers = Object.keys(searchParams)
+    .filter((key) => key.startsWith('header_'))
+    .reduce(
+      (acc, key) => {
+        acc[decode(key.replace('header_', ''))] = Array.isArray(
+          searchParams[key],
+        )
+          ? decode(searchParams[key].join(', '))
+          : decode(searchParams[key]);
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
+  console.log('HH:', headers);
+
+  const fetchResponse = async () => {
+    try {
+      const response = await fetch(endpoint, {
+        method,
+        headers: {
+          ...headers,
+          'Content-Type': method !== 'GET' ? 'application/json' : undefined,
+        },
+        body: method !== 'GET' ? JSON.stringify(body) : undefined,
+      });
+      if (!response.ok) {
+        console.log(await response.json);
+      }
+      const data = await response.json();
+
+      return {
+        status: response.status,
+        statusText: response.statusText,
+        data,
+      };
+    } catch (error) {
+      console.log('zzz', error);
+    }
+  };
+
+  const { status, statusText, data } = await fetchResponse();
+
+  return (
+    <div className={styles.restContainer}>
+      <RestClient />
+      <ResponseSection
+        responseCode={status}
+        responseStatus={statusText}
+        responseBody={data}
+      />
+    </div>
+  );
+}
