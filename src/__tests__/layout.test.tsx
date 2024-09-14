6b7d1fe11f473d670e47a4c8219c0316019db57a
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-09-14 15:04:19 +0400

    fix: import font directly from css file to avoid test failing

---
+++ src/__tests__/layout.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react';
+import RootLayout from '../app/layout';
+import { vi } from 'vitest';
+import ErrorBoundary from '../app/error-boundary';
+import '@testing-library/jest-dom';
+
+// Mock navigation
+vi.mock('next/navigation', () => ({
+  useRouter: () => ({
+    route: '/',
+    pathname: '',
+    query: {},
+    asPath: '',
+    push: vi.fn(),
+    back: vi.fn(),
+  }),
+}));
+
+// Mock ErrorBoundary
+vi.mock('../app/error-boundary', () => ({
+  __esModule: true,
+  default: ({ children }: { children: React.ReactNode }) => <>{children}</>,
+}));
+
+describe('RootLayout', () => {
+  it('renders Header, Footer, and children components correctly', () => {
+    render(
+      <RootLayout>
+        <div>Child Component</div>
+      </RootLayout>,
+    );
+
+    expect(screen.getByText('Child Component')).toBeInTheDocument();
+  });
+
+  it('handles ErrorBoundary correctly', () => {
+    render(
+      <RootLayout>
+        <ErrorBoundary>
+          <div>Child Component</div>
+        </ErrorBoundary>
+      </RootLayout>,
+    );
+
+    expect(screen.getByText('Child Component')).toBeInTheDocument();
+  });
+});
