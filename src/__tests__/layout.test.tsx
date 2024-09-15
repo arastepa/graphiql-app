cdb0a437835232de54f86a638869f714d37dd80b
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-09-15 17:00:28 +0400

    fix: layout file

--- src/__tests__/layout.test.tsx
+++ src/__tests__/layout.test.tsx
@@ -1,46 +1,79 @@
 import { render, screen } from '@testing-library/react';
 import RootLayout from '../app/layout';
 import { vi } from 'vitest';
-import ErrorBoundary from '../app/error-boundary';
+import React from 'react';
 import '@testing-library/jest-dom';
+
+vi.mock('@/components/Header', () => ({
+  default: () => <div>Mock Header</div>,
+}));
+vi.mock('@/components/Footer', () => ({
+  default: () => <div>Mock Footer</div>,
+}));
+vi.mock('@/context/LanguageContext', () => ({
+  LanguageProvider: ({ children }: { children: React.ReactNode }) => (
+    <div>{children}</div>
+  ),
+}));
+vi.mock('@/context/AuthContext', () => ({
+  AuthProvider: ({ children }: { children: React.ReactNode }) => (
+    <div>{children}</div>
+  ),
+}));
+vi.mock('@/context/ResponseContext', () => ({
+  ResponseProvider: ({ children }: { children: React.ReactNode }) => (
+    <div>{children}</div>
+  ),
+}));
+vi.mock('../loading', () => ({
+  default: () => <div>Loading...</div>,
+}));
+
+vi.mock('../error-boundary', () => ({
+  default: ({ children }: { children: React.ReactNode }) => (
+    <div>{children}</div>
+  ),
+}));
 
 vi.mock('next/navigation', () => ({
   useRouter: () => ({
-    route: '/',
-    pathname: '',
-    query: {},
-    asPath: '',
     push: vi.fn(),
+    replace: vi.fn(),
+    prefetch: vi.fn(),
     back: vi.fn(),
+    forward: vi.fn(),
+    refresh: vi.fn(),
+    route: '/',
+    pathname: '/',
+    query: {},
+    asPath: '/',
   }),
 }));
 
-// Mock ErrorBoundary
-vi.mock('../app/error-boundary', () => ({
-  __esModule: true,
-  default: ({ children }: { children: React.ReactNode }) => <>{children}</>,
-}));
-
 describe('RootLayout', () => {
-  it('renders Header, Footer, and children components correctly', () => {
+  test('renders Header, Footer, and children components correctly', () => {
     render(
       <RootLayout>
-        <div>Child Component</div>
+        <div>Mock Child Content</div>
       </RootLayout>,
     );
 
-    expect(screen.getByText('Child Component')).toBeInTheDocument();
+    expect(screen.getByText('Mock Header')).toBeInTheDocument();
+    expect(screen.getByText('Mock Footer')).toBeInTheDocument();
+    expect(screen.getByText('Mock Child Content')).toBeInTheDocument();
   });
 
-  it('handles ErrorBoundary correctly', () => {
+  test('handles ErrorBoundary correctly', () => {
+    const ErrorComponent = () => {
+      throw new Error('Test Error');
+    };
+
     render(
       <RootLayout>
-        <ErrorBoundary>
-          <div>Child Component</div>
-        </ErrorBoundary>
+        <ErrorComponent />
       </RootLayout>,
     );
 
-    expect(screen.getByText('Child Component')).toBeInTheDocument();
+    expect(screen.getByText(/something went wrong/i)).toBeInTheDocument();
   });
 });
